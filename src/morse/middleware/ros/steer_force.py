0088dc0d1bf41bd89b4c9059f1f37b4d905e7e75
David Hodo <david.hodo@gmail.com>
2013-02-20 10:42:21 -0600

    fixing bugs in steer_force ros interface

--- src/morse/middleware/ros/steer_force.py
+++ src/morse/middleware/ros/steer_force.py
@@ -1,5 +1,5 @@
 import roslib; roslib.load_manifest('ackermann_msgs')
-from ackermann.msg import AckermannDrive
+from ackermann_msgs.msg import AckermannDrive
 from morse.middleware.ros import ROSReader
 
 class AckermannDriveReader(ROSReader):
@@ -11,6 +11,6 @@ class AckermannDriveReader(ROSReader):
         if (abs(message.speed)<0.01):
             self.data["force"] = 0
             self.data["brake"] = 10000
-        else
+        else:
             self.data["force"] = message.speed*10000
             self.data["brake"] = 0  
